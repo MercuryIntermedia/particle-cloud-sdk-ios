b10630471cdff1131f968be5d9e7c714e09c43c0
stallent <stallent@mercuryintermedia.com>
2020-04-09 16:54:48 -0500

    initial mac support

---
+++ ParticleSDKOSX/ParticleSDKOSX.h
@@ -0,0 +1,19 @@
+//
+//  ParticleSDKOSX.h
+//  ParticleSDKOSX
+//
+//  Created by Stephen Tallent on 4/9/20.
+//  Copyright © 2020 Particle Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ParticleSDKOSX.
+FOUNDATION_EXPORT double ParticleSDKOSXVersionNumber;
+
+//! Project version string for ParticleSDKOSX.
+FOUNDATION_EXPORT const unsigned char ParticleSDKOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParticleSDKOSX/PublicHeader.h>
+
+
