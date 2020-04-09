e53e3265b3566d917f3acfacbb392c3061fb0b52
stallent <stallent@mercuryintermedia.com>
2020-04-09 17:06:29 -0500

    add the headers

--- ParticleSDKOSX/ParticleSDKOSX.h
+++ ParticleSDKOSX/ParticleSDKOSX.h
@@ -15,5 +15,9 @@ FOUNDATION_EXPORT double ParticleSDKOSXVersionNumber;
 FOUNDATION_EXPORT const unsigned char ParticleSDKOSXVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ParticleSDKOSX/PublicHeader.h>
-
+#import <ParticleSDK/ParticleCloud.h>
+#import <ParticleSDK/ParticleDevice.h>
+#import <ParticleSDK/ParticleEvent.h>
+#import <ParticleSDK/ParticleErrorHelper.h>
+#import <ParticleSDK/ParticleLogger.h>
 
